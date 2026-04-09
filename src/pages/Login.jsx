a7a863f7b99f12ca7689028bdb76a1e2aa25638a
pndkas <pndkas@gmail.com>
2026-04-09 14:56:16 +0700

    feat: login&register

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <div>
+        <div>EMAIL ADDRESS</div>
+      </div>
+      <div>PASSWORD</div>
+    </div>
+  );
+}
+
+export default Login;
