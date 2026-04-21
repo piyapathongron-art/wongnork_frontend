0c8d1c69aa8a57bd87618d0d2f3dca21977d04c1
suphawit135 <prame135@gmail.com>
2026-04-21 15:03:01 +0700

    feature/mail_verify

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,8 @@
-import React, { use, useState } from "react";
+import React, { use, useState,useEffect } from "react";
 import { useForm } from "react-hook-form";
 import useUserStore from "../stores/userStore";
 import { toast, ToastContainer } from "react-toastify";
-import { useNavigate } from "react-router";
+import { useNavigate,useLocation } from "react-router";
 import { loginSchema } from "../validations/schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { GoogleLogin } from "@react-oauth/google";
@@ -13,6 +13,8 @@ const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const login = useUserStore((state) => state.login);
   const googleLogin = useUserStore((state) => state.googleLogin)
+
+  const navigate = useNavigate();
 
   const {
     register,
@@ -26,7 +28,7 @@ const Login = () => {
     },
   });
 
-  const navigate = useNavigate();
+
 
   const submitLogin = async (data) => {
     try {
